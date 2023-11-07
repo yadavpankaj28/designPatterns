2d7528e2a73ef4dab81eb6dd368bfea2a31dd53c
Pankaj Yadav <pankaj.pa.yadav@oracle.com>
2023-11-08 00:43:44 +0530

    comment errored lines

--- src/com/scaler/lecture/observer/AnalyticsSender.java
+++ src/com/scaler/lecture/observer/AnalyticsSender.java
@@ -19,10 +19,10 @@ public class AnalyticsSender implements OrderPlacedSubscriber, OrderCancelledSub
     }
 }
 
-UIEventManager
-
-SubscriberManager {
-    triggerUnreisterEvnt() {
-
-    }
-}
+//UIEventManager
+//
+//SubscriberManager {
+//    triggerUnreisterEvnt() {
+//
+//    }
+//}
